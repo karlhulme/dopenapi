884250a96535c04f2c5630a213f57fd6246ca846
karlhulme <karlhulme@hotmail.com>
2022-06-25 13:52:28 +0100

    RequestClause body handling fixed.

--- src/converter/convertPathOperationCallToFunction.ts
+++ src/converter/convertPathOperationCallToFunction.ts
@@ -87,29 +87,25 @@ function generateUrlClause(pathUrl: string, path: OpenApiSpecPath) {
 }
 
 function generateRequestClause(method: string, op: OpenApiSpecPathOperation) {
-  let block = `const headers: Record<string, string>`;
+  let block = `const headers: Record<string, string> = {};\n`;
 
   for (const param of op.parameters) {
     if (param.in === "header") {
       block += `if (typeof props["${param.name}"] !== "undefined") {\n`;
-      block += `header["${param.name}"] = props["${param.name}"].toString(),\n`;
+      block +=
+        `headers["${param.name}"] = props["${param.name}"].toString();\n`;
       block += "}\n";
     }
   }
 
-  block += `const response = await fetch(\n`;
-  block += "url, {\n";
+  block += `const response = await fetch(url, {\n`;
   block += "...options,\n";
   block += `method: "${method}",\n`;
   block += "headers,\n";
-  block += "}\n";
-
   if (typeof op.requestBody !== "undefined") {
     block += "body: JSON.stringify(props.body),\n";
   }
-
-  block += "}\n";
-  block += ")\n";
+  block += "});\n";
 
   return block;
 }

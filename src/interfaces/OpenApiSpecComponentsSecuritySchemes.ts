3c13fdf5fcb1deeeace3c600c0da996025c873f1
Karl Hulme <karlhulme@hotmail.com>
2023-05-20 16:15:00 +0100

    fix: drop cookieAuth as security schema because it cannot be implemented by tooling

--- src/interfaces/OpenApiSpecComponentsSecuritySchemes.ts
+++ src/interfaces/OpenApiSpecComponentsSecuritySchemes.ts
@@ -3,6 +3,11 @@ import { OpenApiSpecComponentsSecuritySchemesApiKey } from "./OpenApiSpecCompone
 /**
  * Represents a set of security schemes that will be referenced
  * by the operations.
+ *
+ * Cookie is not supported since this should not be handled
+ * manually by developers and tools are hampered in their ability
+ * to set cookies manually because of the security restrictions
+ * placed upon browsers.
  */
 export interface OpenApiSpecComponentsSecuritySchemes {
   /**
@@ -10,10 +15,4 @@ export interface OpenApiSpecComponentsSecuritySchemes {
    * uses an API key in the header for security.
    */
   apiKeyAuth?: OpenApiSpecComponentsSecuritySchemesApiKey;
-
-  /**
-   * If populated, the presence of this key indicates the service
-   * uses an API key in a cookie for security.
-   */
-  cookieAuth?: OpenApiSpecComponentsSecuritySchemesApiKey;
 }
